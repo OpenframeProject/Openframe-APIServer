d595f6dfef27193009120ffb47c21031d54992d8
Jonathan Wohl <jon@jonwohl.com>
2016-10-09 17:57:14 -0500

    Add default pubsub url

--- server/config.local.js
+++ server/config.local.js
@@ -26,7 +26,7 @@ module.exports = {
         }
     },
     'webapp_url': process.env.WEBAPP_EXPOSED_URL || null,
-    'pubsub_url': process.env.PS_EXPOSED_URL || null,
+    'pubsub_url': process.env.PS_EXPOSED_URL || 'http://0.0.0.0:8889/faye',
     'pubsub_api_token': process.env.PS_API_TOKEN || '8629c101-4db7-4a0a-9bf1-33c5fc3cff23',
     'cookieSecret': process.env.COOKIE_SECRECT || 'cce6829c-328e-4e38-aa5c-f4714ac93914',
     'legacyExplorer': false
